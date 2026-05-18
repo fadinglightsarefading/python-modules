4fe01edcc7b93ecaf962ff2256acdfaaccc6ff2b
Christian Frederico Clarke <cclarke@c3r3s1.42prague.com>
2026-05-18 20:10:59 +0200

    crumb

--- m7/tournament.py
+++ m7/tournament.py
@@ -1,9 +1,20 @@
 from ex0 import CreatureFactory, FlameFactory, AquaFactory
 from ex1 import HealingCreatureFactory, TransformCreatureFactory
-from ex2 import NormalStrategy, AggressiveStrategy, DefensiveStrategy
+from ex2 import BattleStrategy, NormalStrategy, AggressiveStrategy, DefensiveStrategy
+
+
+def battle(opponents: list[tuple[CreatureFactory, BattleStrategy]]) -> None:
+    
+
 
 def main() -> None:
-    pass
+    flame_fac = FlameFactory()
+    aqua_fac = AquaFactory()
+    hc_fac = HealingCreatureFactory()
+    tc_fac = TransformCreatureFactory()
+    normal_strat = NormalStrategy()
+    agress_strat = AggressiveStrategy()
+    defens_strat = DefensiveStrategy()
 
 
 if __name__ == "__main__":
